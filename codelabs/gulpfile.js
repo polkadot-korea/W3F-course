9e3e648f26e4ae2d3976c7430633c6eb12648c9f
hskang9 <hskang9@gmail.com>
2019-11-21 01:59:42 +0900

    Updates

--- codelabs/gulpfile.js
+++ codelabs/gulpfile.js
@@ -948,9 +948,9 @@ const ghPages = require('gh-pages');
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function() {
+gulp.task('deploy', function(callback) {
   ghPages.publish('build', {
     branch: 'gh-pages',
     repo: 'https://github.com/polkadot-korea/tools.git'
-  });
+  }, callback);
 });
